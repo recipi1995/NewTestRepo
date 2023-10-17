a3e7ab9c51490e78eeb63a3138d0ffa25b66e08b
Ivan Malyshev <malyshev.ivan@inbox.ru>
2023-10-17 22:34:01 +0300

    Initial Commit

---
+++ NewTestRepo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewTestRepo
+//
+//  Created by Иван Малышев on 17.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
